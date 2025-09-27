f3ded8ee7a1f1768feb5fe77baf4c12b9df5f2b4
peter vol <petervol35@gmail.com>
2025-09-27 19:24:02 +0300

    fuck css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
 
-function Home() {
-  return <h1>Home</h1>;
-}
 function About() {
   return <h1>About</h1>;
 }
@@ -15,20 +14,42 @@ function Gallery() {
 
 export default function App() {
   return (
-    <HashRouter>
-      <nav style={{ display: 'flex', gap: 12 }}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/pricing">Pricing</Link>
-        <Link to="/gallery">Gallery</Link>
-      </nav>
+    <div className="app">
+      <HashRouter>
+        {/* Navbar */}
+        <nav className="navbar-custom">
+          <div className="d-flex flex-column align-items-center">
+            {/* Logo */}
+            <h4 className="logo">🔧</h4>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/gallery" element={<Gallery />} />
-      </Routes>
-    </HashRouter>
+            {/* Links */}
+            <div className="nav-links">
+              <NavLink to="/" end className="nav-link-custom">
+                Home ▼
+              </NavLink>
+              <NavLink to="/about" className="nav-link-custom">
+                About ▼
+              </NavLink>
+              <NavLink to="/pricing" className="nav-link-custom">
+                Pricing ▼
+              </NavLink>
+              <NavLink to="/gallery" className="nav-link-custom">
+                Gallery ▼
+              </NavLink>
+            </div>
+          </div>
+        </nav>
+
+        {/* Page content */}
+        <main className="content-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/gallery" element={<Gallery />} />
+          </Routes>
+        </main>
+      </HashRouter>
+    </div>
   );
 }
